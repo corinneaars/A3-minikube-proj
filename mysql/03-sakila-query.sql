7e2d6887ea5cf81b61a5f07307dc2eb0c6122643
corinneaars <100172412+corinneaars@users.noreply.github.com>
2024-03-18 20:51:00 -0500

    Delete mysql/03-sakila-query.sql

--- mysql/03-sakila-query.sql
+++
@@ -1,7 +0,0 @@
-SELECT first_name, last_name, email, country.country, city.city
-FROM customer
-LEFT JOIN address ON address.address_id=customer.address_id
-LEFT JOIN city ON city.city_id=address.city_id
-LEFT JOIN country ON city.country_id=country.country_id
-WHERE country.country="Canada"
-ORDER BY city.city;
